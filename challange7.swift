fddfee5df2642eb16b7982d8d9aab305af8d370c
Attila Olah <NoNameProvided@users.noreply.github.com>
2019-08-22 13:49:49 +0200

    style: fix identiation in challenge 7

--- challange7.swift
+++ challange7.swift
@@ -6,60 +6,59 @@ import AppKit
 extension NSImage {
 
   func pixelData() -> [Pixel] {
-        let bmp = self.representations[0] as! NSBitmapImageRep
-        var data: UnsafeMutablePointer<UInt8> = bmp.bitmapData!
-        var r, g, b, a: UInt8
-        var pixels: [Pixel] = []
+    let bmp = self.representations[0] as! NSBitmapImageRep
+    var data: UnsafeMutablePointer<UInt8> = bmp.bitmapData!
+    var r, g, b, a: UInt8
+    var pixels: [Pixel] = []
 
-        for y in 0..<bmp.pixelsHigh {
-            for x in 0..<bmp.pixelsWide {
-                r = data.pointee
-                data = data.advanced(by: 1)
-                g = data.pointee
-                data = data.advanced(by: 1)
-                b = data.pointee
-                data = data.advanced(by: 1)
-                a = data.pointee
-                pixels.append(Pixel(r: r, g: g, b: b, a: a, y: y, x: x))
-            }
+    for y in 0..<bmp.pixelsHigh {
+        for x in 0..<bmp.pixelsWide {
+            r = data.pointee
+            data = data.advanced(by: 1)
+            g = data.pointee
+            data = data.advanced(by: 1)
+            b = data.pointee
+            data = data.advanced(by: 1)
+            a = data.pointee
+            pixels.append(Pixel(r: r, g: g, b: b, a: a, y: y, x: x))
         }
+    }
 
-      return pixels
-   }
+    return pixels
+  }
 
-   func getPixelAt(x: Int, y: Int) -> NSColor {
+  func getPixelAt(x: Int, y: Int) -> NSColor {
     let bmp = self.representations[0] as! NSBitmapImageRep
 
     return bmp.colorAt(x: x, y: y)!
-   }
+  }
 
 }
 
 struct Pixel {
+  var r: Float
+  var g: Float
+  var b: Float
+  var a: Float
+  var y: Int
+  var x: Int
 
-   var r: Float
-   var g: Float
-   var b: Float
-   var a: Float
-   var y: Int
-   var x: Int
+  init(r: UInt8, g: UInt8, b: UInt8, a: UInt8, y: Int, x: Int) {
+    self.r = Float(r)
+    self.g = Float(g)
+    self.b = Float(b)
+    self.a = Float(a)
+    self.y = y
+    self.x = x
+  }
 
-   init(r: UInt8, g: UInt8, b: UInt8, a: UInt8, y: Int, x: Int) {
-      self.r = Float(r)
-      self.g = Float(g)
-      self.b = Float(b)
-      self.a = Float(a)
-      self.y = y
-      self.x = x
-   }
+  var color: NSColor {
+    return NSColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a/255.0))
+  }
 
-   var color: NSColor {
-      return NSColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a/255.0))
-   }
-
-   var description: String {
-      return "RGBA(\(r), \(g), \(b), \(a))"
-   }
+  var description: String {
+    return "RGBA(\(r), \(g), \(b), \(a))"
+  }
 
 }
 
